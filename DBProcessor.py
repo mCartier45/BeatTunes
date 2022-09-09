f1d146a34aa0cf453b58849be2d8fac36e76f1c4
Singroff <caleb.epps@yahoo.com>
2022-09-09 17:28:18 -0400

    - Condensed all databases into single database - Single database is now parseable by year TODO: Rename database and add better method to check if database already exists

--- DBProcessor.py
+++ DBProcessor.py
@@ -2,25 +2,25 @@ import os
 import sqlite3
 
 
-
 class DBOps:
 
-    def __init__(self, db):
-        self.connect = sqlite3.connect("data/" + db + ".db")
+    def __init__(self):
+        self.connect = sqlite3.connect("data/condensed.db")
         self.cursor = self.connect.cursor()
 
-
-    def initialize_db(self, playlist):
+    def initialize_db(self):
         # file = open("data/" + playlist + ".db", "x")
         self.cursor.execute('''
           CREATE TABLE IF NOT EXISTS songs
-          (title TEXT primary key, 
+          (title TEXT, 
            key TEXT,
            tempo INTEGER,
            danceability INTEGER,
            loudness INTEGER,
            acousticness INTEGER,
-           duration_ms INTEGER)
+           duration_ms INTEGER,
+           year INTEGER,
+           uri TEXT primary key)
           ''')
         self.connect.commit()
 
@@ -28,7 +28,6 @@ class DBOps:
         keys = {0: "C", 1: "C#", 2: "D", 3: "D#", 4: "E",
                 5: "F", 6: "F#", 7: "G", 8: "G#", 9: "A",
                 10: "B Flat", 11: "B"}
-
         for song in song_dict:
             bpm = song_dict[song].get("bpm")
             key = keys.get(song_dict[song].get("key"))
@@ -37,43 +36,47 @@ class DBOps:
             dance = song_dict[song].get("danceability")
             title = song_dict[song].get("title")
             duration_ms = song_dict[song].get("duration_ms")
-            speech = song_dict[song].get("speechiness")
+            uri = song_dict[song].get("uri")
+            year = song_dict[song].get("year")
 
-            print(bpm, key, loudness, acoustic, dance, title, duration_ms)
+            print(bpm, key, loudness, acoustic, dance, title, duration_ms, uri)
 
-            # Need to place these into the order that the values are inserted
-            q = f"INSERT INTO songs VALUES ('{title}', '{key}','{bpm}','{dance}','{loudness}', '{acoustic}', '{duration_ms}')"
+            # Switch away from this to disallow sql injection
+            q = "INSERT INTO songs VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?)"
 
-            self.cursor.execute(q)
+            self.cursor.execute(q, [title, key, bpm, dance, loudness, acoustic, duration_ms, year, uri])
             self.connect.commit()
 
-    def print_db(self):
+    def print_all_bpms(self):
 
-        for x in self.cursor.execute("select BPM from songs"):
+        for x in self.cursor.execute("select tempo, title from songs"):
+            print("-------------------------------")
+            print("TITLE: ", x[1])
+            print("BPM: ", x[0])
+            print("-------------------------------")
+
+    def print_all_uris(self):
+        for x in self.cursor.execute("select uri from songs"):
             print(x[0])
 
-    def get_avg_bpm(self):
-        total = 0
-        average = 0
-        result = 0
-        for x in self.cursor.execute("select BPM from songs"):
-            result += float(x[0])
-            total += 1
+    def get_avg_bpm(self, year):
+        for x in self.cursor.execute("select avg(tempo) from songs where year=" + year):
+            print(x[0])
+            return x[0]
 
-        average = result / total # Basic Avg calculation
-        print("The average BPM for the Billboard Top 100 in 1972 is: %.3f" % average)
-
-        return average
+    def debug_sql(self, query):
+        for x in self.cursor.execute(query):
+            print(x[0])
 
     def get_most_common_key(self):
         most_common = "Default Key"
         list_of_keys = []
         counter = 0
 
-        for x in self.cursor.execute("select key from songs"): # Append to List
+        for x in self.cursor.execute("select key from songs"):  # Append to List
             list_of_keys.append(x[0])
 
-        for x in list_of_keys: # Loop list and find most common
+        for x in list_of_keys:  # Loop list and find most common
             current_frequency = list_of_keys.count(x)
 
             if current_frequency > counter:
@@ -93,6 +96,7 @@ class DBOps:
         else:
             return False
 
+
 if __name__ == "__main__":
 
     # read_j.replace_single_quotes(file="FeatSong0.json")
@@ -106,9 +110,5 @@ if __name__ == "__main__":
     db_ops.print_db()
     db_ops.get_avg_bpm()
     db_ops.get_most_common_key()
-
-
-
-
 
     db_ops.close_db()
--- PlaylistParsing.py
+++ PlaylistParsing.py
@@ -46,7 +46,7 @@ class PlaylistProcessor:
 
     # TODO: Use this method to return a dictionary of relevant information
     # and save it to the database.
-    def extract_song_information(self, uri_list, title_list):
+    def extract_song_information(self, uri_list, title_list, playlist_name):
         decade_dict = {}
 
         for x in range(len(uri_list)):
@@ -60,28 +60,43 @@ class PlaylistProcessor:
                                         "duration_ms": temp_features[0]['duration_ms'],
                                         "key": temp_features[0]['key'],
                                         "instrumentalness": temp_features[0]['instrumentalness'],
-                                        "title": title_list[x]}
+                                        "title": title_list[x],
+                                        "uri": uri_list[x],
+                                        "year": self.get_year(playlist_name)}
         return decade_dict
+
+    def get_year(self, playlist_name):
+        decade_dict = {'Top Billboard Hits 1950s': 1950, 'Top Billboard Hits 1960s': 1960,
+                       'Top Billboard Hits 1970s': 1970, 'Top Billboard Hits 1980s': 1980,
+                       'Top Billboard Hits 1990s': 1990, 'Top Billboard Hits 2000s': 2000,
+                       'Top Billboard Hits 2010s': 2010, 'Top Billboard Hits 2020s': 2020}
+        return decade_dict.get(playlist_name)
 
 
 if __name__ == "__main__":
 
     test = PlaylistProcessor()
     playlists = test.get_playlists()
+    db_ops = DBProcessor.DBOps()
 
     print("Looking for Playlists")
-    for x in playlists['items']:
-        print("----------------------")
-        print(x['name'])
-        print("----------------------")
+    if not os.path.exists("data/condensed.db"):
+        for x in playlists['items']:
+            playlist_name = x['name']
+            print("----------------------")
+            print(playlist_name)
+            print("----------------------")
 
-        db_ops = DBProcessor.DBOps(x['name'])
+            uris, titles = test.get_uris_and_titles(x['uri'])
 
-        uris, titles = test.get_uris_and_titles(x['uri'])
+            db_ops.initialize_db()
 
-        db_ops.initialize_db(x['name'])
+            # This line does it ALL
+            new_dict = test.extract_song_information(uris, titles, playlist_name)
+            print(new_dict)
+            db_ops.add_songs_to_db(new_dict)
 
-        # This line does it ALL
-        new_dict = test.extract_song_information(uris, titles)
-        print(new_dict)
-        db_ops.add_songs_to_db(new_dict)
+    db_ops.print_all_bpms()
+
+    print("Average BPM for year 1950: %d" % db_ops.get_avg_bpm(year="1980"))
+    print(db_ops.debug_sql(query="select key from songs where year=1970"))
