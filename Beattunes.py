675797b14c7fc2af79080ad8d39b6da54654a18c
Singroff <caleb.epps@yahoo.com>
2022-09-12 22:10:58 -0400

    - Committed Database for remote work

--- Beattunes.py
+++ Beattunes.py
@@ -3,6 +3,7 @@ import os
 import DBProcessor
 from PlaylistParsing import PlaylistProcessor
 
+
 if __name__ == "__main__":
 
     process_playlist = PlaylistProcessor()
@@ -17,13 +18,10 @@ if __name__ == "__main__":
     db_ops.print_all_bpms()
 
     file = open("data/years.json")
-    year = json.load(file)
+    years = json.load(file)
 
-    for x in year[0]:
-        print(year[0][x])
-
-    for x in year[0]:
-        print("The average BPM for " + str(year[0][x]) + " is: %d" % db_ops.get_avg_bpm(year=year[0][x]))
+    for x in years[0]:
+        print("The average BPM for " + str(years[0][x]) + " is: %d" % db_ops.get_avg_bpm(year=years[0][x]))
 
     print("The most common key in 2020 was: " + db_ops.get_most_common_key(year="2020"))
 
